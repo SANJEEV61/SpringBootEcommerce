69041c64f1eea685fcbb4d3a1f31f1ec3e379217
sanjeevr06 <sanjeev06022001@gmail.com>
2025-09-21 11:54:06 +0530

    initial commit3

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,21 +2,19 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class CategoryContoller {
+public class CategoryController {
+    @Autowired
     private CategoryService categoryService;
 
-    public CategoryContoller(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
 
     @GetMapping("api/public/categories")
    public List<Category> getAllCategories(){
